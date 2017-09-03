9f3ad0260c6229fd5eb5b0a710a71963ea9ccbb0
Gabriel Rohden <gabriel_rohden@outlook.com>
2017-09-02 23:07:48 -0300

    Changed tasks and added tests tests

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@
 const fs = require('fs');
 const pug = require('gulp-pug');
 const gulp = require('gulp');
+const Server = require('karma').Server;
 const babel = require("gulp-babel");
 const watch = require('gulp-watch');
 const concat = require('gulp-concat');
@@ -24,73 +25,95 @@ const VIEWS_EXTENSION = 'pug';
 const VIEWS_SRC_FOLDER = './demo/doggos';
 const VIEWS_DEST_FOLDER = './demo/';
 const SCRIPTS_EXTENSION = 'js';
+const KARMA_CONF = __dirname + '/karma.conf.js';
 
 /*-----------------*\
         Tasks
 \*-----------------*/
 
+gulp.task('default', ['build-all'])
 
 //- Views
 gulp.task('watch-views', watchViews);
 
-gulp.task('compile-views', compileViews);
+gulp.task('build-views', buildViews);
 
 //- Scripts
 gulp.task('watch-scripts', watchScripts);
 
-gulp.task('compile-scripts', compileScripts);
+gulp.task('build-scripts', buildScripts);
+
+//- Tests
+
+gulp.task('watch-tests', watchTests);
+
+gulp.task('run-tests', runTests);
 
 //- All
-gulp.task('compile-all', ['compile-views', 'compile-scripts']);
+gulp.task('build-all', ['build-views', 'run-tests', 'build-scripts']);
 
 gulp.task('watch-all', ['watch-views', 'watch-scripts']);
-
 
 /*-----------------*\
       Functions
 \*-----------------*/
+function runTests(done) {
+  new Server({
+    configFile: KARMA_CONF,
+    singleRun: true
+  }, done)
+    .start();
+}
+
+function watchTests(done) {
+  new Server({
+    configFile: KARMA_CONF,
+    singleRun: false
+  }, done).start();
+}
 
 //- Demo Views
-function compileViews() {
-    gulpUtil.log(`compiling views to build/`);
+function buildViews() {
+  gulpUtil.log(`compiling views to build/`);
 
-    return gulp.src(`${VIEWS_SRC_FOLDER}/index.${VIEWS_EXTENSION}`)
-        .pipe(pug({
-            pretty: true
-        }))
-        .pipe(gulp.dest(`${VIEWS_DEST_FOLDER}`));
+  return gulp.src(`${VIEWS_SRC_FOLDER}/*.pug`)
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest(`${VIEWS_DEST_FOLDER}`));
 }
 
 function watchViews() {
-    gulp.watch(`${VIEWS_SRC_FOLDER}/*.${VIEWS_EXTENSION}`, ['compile-views'])
-        .on('change', function (event) {
-            gulpUtil.log(`File ${event.path} was ${event.type}, running views compile..`);
-        });
+  gulp.watch(`${VIEWS_SRC_FOLDER}/*.${VIEWS_EXTENSION}`, ['build-views'])
+    .on('change', function (event) {
+      gulpUtil.log(`File ${event.path} was ${event.type}, running views build..`);
+    });
 
 
 }
 
 //- Scripts
-function compileScripts() {
-    gulpUtil.log('compiling scripts to build/js ');
-    return gulp
-        .src([
-            './src/grHourFormat.module.js',
-            './src/floatToHour.filter.js',
-            './src/formatToMillis.filter.js',
-            './src/grHourFormat.controller.js',
-            './src/grHourFormat.directive.js'
-        ])
-        .pipe(embeddedTemplates())
-        .pipe(concat('grHourInput.min.js'))
-        .pipe(babel())
-        .pipe(gulpUtil.env.type == "production" ? uglify() : gulpUtil.noop())
-        .pipe(gulp.dest('build/'))
+function buildScripts() {
+  gulpUtil.log('compiling scripts to build/js ');
+  return gulp
+    .src([
+      './src/grHourFormat.module.js',
+      './src/floatToHour.filter.js',
+      './src/formatToMillis.filter.js',
+      './src/grHourFormat.controller.js',
+      './src/grHourFormat.directive.js'
+    ])
+
+    .pipe(embeddedTemplates())
+    .pipe(concat('grHourInput.min.js'))
+    .pipe(babel())
+    .pipe(gulpUtil.env.type == "production" ? uglify() : gulpUtil.noop())
+    .pipe(gulp.dest('build/'))
 }
 
 function watchScripts() {
-    gulp.watch(`src/*.${SCRIPTS_EXTENSION}`, ['compile-scripts'])
-        .on('change', function (event) {
-            gulpUtil.log(`File ${event.path} was ${event.type}, running scripts compile..`);
-        });
+  gulp.watch(`src/*.${SCRIPTS_EXTENSION}`, ['build-scripts'])
+    .on('change', function (event) {
+      gulpUtil.log(`File ${event.path} was ${event.type}, running scripts build..`);
+    });
 }
